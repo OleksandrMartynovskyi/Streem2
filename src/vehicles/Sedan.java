3d72965fc8fc5c99ca050040fd223fd7ec0c8034
Aleksandr <0974111411m@gmail.com>
2023-12-11 19:33:56 +0200

    Changes

---
+++ src/vehicles/Sedan.java
@@ -0,0 +1,14 @@
+package vehicles;
+
+public class Sedan extends Vehicle{
+    public Sedan(String brand, String model, int year, double price, String color) {
+        super(brand, model, year, price, color);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Sedan{");
+        sb.append('}');
+        return sb.toString();
+    }
+}
